RustAudio/vst-rs

commit b124fefab71db5e5f3bfb4e293d3756f2fba810a

Run clippy in CI
Hope that it not requires to call `rustup update` beforehand to keep
this step resasonably fast.

Install nightly toolchain

This is required for clippy and probably rustfmt, too.

Install clippy and rustfmt in one step

Provide a basic Makefile

It has targets to build, test, check i.e. lint and codestyle-check as
well as clean the project.

Apply clippy lints

Remove redundant field-names

use `ptr::null_mut()`

Dereference self instead of matching against references

Replace transmutes

Allow pointer cast allignment warnings

They should be fixed but this is to make the CI happy and get clippy
checks merged.

src/plugin.rs
@@ -1,6 +1,6 @@
 //! Plugin specific structures.
 
-use std::{mem, ptr};
+use std::ptr;
 
 use std::os::raw::c_void;
 
@@ -802,7 +802,7 @@ impl HostCallback {
     #[doc(hidden)]
     fn is_effect_valid(&self) -> bool {
         // Check whether `effect` points to a valid AEffect struct
-        unsafe { *mem::transmute::<*mut AEffect, *mut i32>(self.effect) == VST_MAGIC }
+        self.effect as i32 == VST_MAGIC
     }
 
     /// Create a new Host structure wrapping a host callback.
@@ -810,7 +810,7 @@ impl HostCallback {
     pub fn wrap(callback: HostCallbackProc, effect: *mut AEffect) -> HostCallback {
         HostCallback {
             callback: Some(callback),
-            effect: effect,
+            effect,
         }
     }
 
@@ -942,7 +942,7 @@ impl Host for HostCallback {
     fn get_time_info(&self, mask: i32) -> Option<TimeInfo> {
         let opcode = host::OpCode::GetTime;
         let mask = mask as isize;
-        let null = 0 as *mut c_void;
+        let null = ptr::null_mut();
         let ptr = self.callback(self.effect, opcode, 0, mask, null, 0.0);
 
         match ptr {

.travis.yml
@@ -1,4 +1,10 @@
 language: rust
+script:
+  - rustup toolchain install nightly
+  - rustup component add --toolchain=nightly clippy-preview
+  - cargo +nightly clippy
+  - cargo build --verbose
+  - cargo test --verbose
 after_success: 
   - |
     [ $TRAVIS_BRANCH = master ] &&

Makefile
@@ -1,0 +1,25 @@
+RUST_SRC:=$(wildcard **/*.rs)
+
+all: build
+
+test: check
+	@cargo test
+
+check: fmt-check lint
+
+build: target/release/libvst.rlib
+
+target/release/libvst.rlib: $(RUST_SRC)
+	@cargo build --release
+
+lint: $(RUST_SRC)
+	@cargo +nightly clippy
+
+fmt: $(RUST_SRC)
+	@cargo +nightly fmt
+
+fmt-check: $(RUST_SRC)
+	@cargo +nightly fmt -- --check
+
+clean:
+	@rm -fr target/

src/buffer.rs
@@ -28,7 +28,7 @@ impl<'a, T: 'a + Float> AudioBuffer<'a, T> {
         Self {
             inputs: slice::from_raw_parts(inputs_raw, input_count),
             outputs: slice::from_raw_parts_mut(outputs_raw, output_count),
-            samples: samples,
+            samples,
         }
     }
 
@@ -313,14 +313,14 @@ impl<'a> WriteIntoPlaceholder for SysExEvent<'a> {
 
 impl<'a> WriteIntoPlaceholder for Event<'a> {
     fn write_into(&self, out: &mut PlaceholderEvent) {
-        match self {
-            &Event::Midi(ref ev) => {
+        match *self {
+            Event::Midi(ref ev) => {
                 ev.write_into(out);
             }
-            &Event::SysEx(ref ev) => {
+            Event::SysEx(ref ev) => {
                 ev.write_into(out);
             }
-            &Event::Deprecated(e) => {
+            Event::Deprecated(e) => {
                 let out = unsafe { &mut *(out as *mut _ as *mut _) };
                 *out = e;
             }
@@ -366,10 +366,7 @@ impl SendEventBuffer {
                 *ptr = event;
             }
         }
-        Self {
-            buf: buf,
-            api_events: api_events,
-        }
+        Self { buf, api_events }
     }
 
     /// Sends events to the host. See the `fwd_midi` example.
@@ -417,12 +414,18 @@ impl SendEventBuffer {
 
     #[inline(always)]
     fn events(&self) -> &api::Events {
-        unsafe { &*(self.buf.as_ptr() as *const api::Events) }
+        #[allow(cast_ptr_alignment)]
+        unsafe {
+            &*(self.buf.as_ptr() as *const api::Events)
+        }
     }
 
     #[inline(always)]
     fn buf_as_api_events(buf: &mut [u8]) -> &mut api::Events {
-        unsafe { &mut *(buf.as_mut_ptr() as *mut api::Events) }
+        #[allow(cast_ptr_alignment)]
+        unsafe {
+            &mut *(buf.as_mut_ptr() as *mut api::Events)
+        }
     }
 
     #[inline(always)]

src/channels.rs
@@ -35,7 +35,7 @@ impl ChannelInfo {
                 name
             },
 
-            active: active,
+            active,
 
             arrangement_type: arrangement_type.unwrap_or(SpeakerArrangementType::Custom),
         }

src/event.rs
@@ -114,7 +114,9 @@ impl<'a> From<api::Event> for Event<'a> {
                 payload: unsafe {
                     // We can safely transmute the event pointer to a `SysExEvent` pointer as
                     // event_type refers to a `SysEx` type.
-                    let event: &api::SysExEvent = mem::transmute(&event);
+                    #[allow(cast_ptr_alignment)]
+                    let event: &api::SysExEvent =
+                        &*(&event as *const api::Event as *const api::SysExEvent);
                     slice::from_raw_parts(event.system_data, event.data_size as usize)
                 },
 

src/host.rs
@@ -344,7 +344,7 @@ impl<T: Host> PluginLoader<T> {
                 }
             },
             lib: Arc::new(lib),
-            host: host,
+            host,
         })
     }
 
@@ -389,8 +389,8 @@ impl PluginInstance {
         use plugin::OpCode as op;
 
         let mut plug = PluginInstance {
-            effect: effect,
-            lib: lib,
+            effect,
+            lib,
             info: Default::default(),
         };
 

src/interfaces.rs
@@ -8,8 +8,8 @@ use std::os::raw::{c_char, c_void};
 
 use buffer::AudioBuffer;
 use api::consts::*;
-use api::{self, AEffect, ChannelProperties, TimeInfo};
-use editor::{Rect, KeyCode, Key, KnobMode};
+use api::{self, AEffect, TimeInfo};
+use editor::{Key, KeyCode, KnobMode, Rect};
 use host::Host;
 
 /// Deprecated process function.
@@ -221,7 +221,7 @@ pub fn dispatch(
         OpCode::GetInputInfo => {
             if index >= 0 && index < plugin.get_info().inputs {
                 unsafe {
-                    let ptr = mem::transmute::<_, *mut ChannelProperties>(ptr);
+                    let ptr = ptr as *mut api::ChannelProperties;
                     *ptr = plugin.get_input_info(index).into();
                 }
             }
@@ -229,7 +229,7 @@ pub fn dispatch(
         OpCode::GetOutputInfo => {
             if index >= 0 && index < plugin.get_info().outputs {
                 unsafe {
-                    let ptr = mem::transmute::<_, *mut ChannelProperties>(ptr);
+                    let ptr = ptr as *mut api::ChannelProperties;
                     *ptr = plugin.get_output_info(index).into();
                 }
             }

commit b229e0831ea464a7a8fb913ca177939aeca0a975

Fix is_effect_valid check which broke in b124fefab71db5e5f3bfb4e293d3756f2fba810a

src/plugin.rs
@@ -802,7 +802,7 @@ impl HostCallback {
     #[doc(hidden)]
     fn is_effect_valid(&self) -> bool {
         // Check whether `effect` points to a valid AEffect struct
-        self.effect as i32 == VST_MAGIC
+        unsafe { *(self.effect as *mut i32) == VST_MAGIC }
     }
 
     /// Create a new Host structure wrapping a host callback.