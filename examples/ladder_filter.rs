b4a9d90553c6afd6cbb9eda4eaf632f5beeb05f5
Fredemus <46689307+Fredemus@users.noreply.github.com>
2019-06-20 19:03:41 +0200

    Update ladder_filter.rs

--- examples/ladder_filter.rs
+++ examples/ladder_filter.rs
@@ -1,206 +1,169 @@
-//!This zero-delay feedback filter is based on a 4-stage transistor ladder filter.
-//!It follows the following equations: 
-//!x = input - tanh(self.res * self.vout[3])
-//!vout[0] = self.g * (tanh(x) - tanh(self.vout[0])) + self.s[0]
-//!vout[1] = self.g * (tanh(self.vout[0]) - tanh(self.vout[1])) + self.s[1]
-//!vout[0] = self.g * (tanh(self.vout[1]) - tanh(self.vout[2])) + self.s[2]
-//!vout[0] = self.g * (tanh(self.vout[2]) - tanh(self.vout[3])) + self.s[3]
-//!since we can't easily solve a nonlinear equation, 
-//!Mystran's fixed-pivot method is used to approximate the tanh() parts. 
-//!Quality can be improved a lot by oversampling a bit. 
-//!Feedback is clipped independently of the input, so it doesn't disappear at high gains.
-        
- #[macro_use] extern crate vst;
+//! This zero-delay feedback filter is based on a 4-stage transistor ladder filter.
+//! It follows the following equations:
+//! x = input - tanh(self.res * self.vout[3])
+//! vout[0] = self.params.g.get() * (tanh(x) - tanh(self.vout[0])) + self.s[0]
+//! vout[1] = self.params.g.get() * (tanh(self.vout[0]) - tanh(self.vout[1])) + self.s[1]
+//! vout[0] = self.params.g.get() * (tanh(self.vout[1]) - tanh(self.vout[2])) + self.s[2]
+//! vout[0] = self.params.g.get() * (tanh(self.vout[2]) - tanh(self.vout[3])) + self.s[3]
+//! since we can't easily solve a nonlinear equation,
+//! Mystran's fixed-pivot method is used to approximate the tanh() parts.
+//! Quality can be improved a lot by oversampling a bit.
+//! Feedback is clipped independently of the input, so it doesn't disappear at high gains.
+
+#[macro_use]
+extern crate vst;
+use std::f32::consts::PI;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+
 use vst::buffer::AudioBuffer;
-use vst::plugin::{Info, Plugin, Category};
+use vst::plugin::{Category, Info, Plugin, PluginParameters};
+use vst::util::AtomicFloat;
 
-#[derive(PartialEq)]
-enum Method {
-    Linear,  // linear solution
-    Pivotal, // Mystran's "cheap" method, using x=0 as pivot
-}
-
-//this is a 4-pole filter with resonance, which is why there's 4 states and vouts
+// this is a 4-pole filter with resonance, which is why there's 4 states and vouts
 #[derive(Clone)]
 struct LadderFilter {
-    //the output of the different filter stages
+    // Store a handle to the plugin's parameter object.
+    params: Arc<LadderParameters>,
+    // the output of the different filter stages
     vout: [f32; 4],
-    //s is the "state" parameter. In an IIR it would be the last value from the filter
-    //In this we find it by trapezoidal integration to avoid the unit delay
+    // s is the "state" parameter. In an IIR it would be the last value from the filter
+    // In this we find it by trapezoidal integration to avoid the unit delay
     s: [f32; 4],
-    //the "cutoff" parameter. Determines how heavy filtering is
-    cutoff: f32,
-    g: f32,
-    //needed to calculate cutoff. 
-    sample_rate: f32,
-    //makes a peak at cutoff
-    res: f32,
-    //used to choose if we want it to output 1 or 2 order filtering
-    poles: usize,
-    //a drive parameter. Just used to increase the volume, which results in heavier distortion
-    drive: f32,
 }
-//member methods for the struct
-impl LadderFilter {
-    pub fn set_cutoff(&mut self, value: f32) {
-        //cutoff formula gives us a natural feeling cutoff knob that spends more time in the low frequencies
-        self.cutoff = 20000. * (1.8f32.powf(10. * value - 10.));
-        //bilinear transformation for g gives us a very accurate cutoff
-        self.g = (3.1415 * self.cutoff / (self.sample_rate)).tan();
+struct LadderParameters {
+    // the "cutoff" parameter. Determines how heavy filtering is
+    cutoff: AtomicFloat,
+    g: AtomicFloat,
+    // needed to calculate cutoff.
+    sample_rate: AtomicFloat,
+    // makes a peak at cutoff
+    res: AtomicFloat,
+    // used to choose where we want our output to be
+    poles: AtomicUsize,
+    // a drive parameter. Just used to increase the volume, which results in heavier distortion
+    drive: AtomicFloat,
+}
+impl Default for LadderParameters {
+    fn default() -> LadderParameters {
+        LadderParameters {
+            cutoff: AtomicFloat::new(1000.),
+            res: AtomicFloat::new(2.),
+            poles: AtomicUsize::new(3),
+            drive: AtomicFloat::new(0.),
+            sample_rate: AtomicFloat::new(44100.),
+            g: AtomicFloat::new(0.07135868087),
+        }
     }
-    //the state needs to be updated after each process. Found by trapezoidal integration
+}
+// member methods for the struct
+impl LadderFilter {
+    // the state needs to be updated after each process. Found by trapezoidal integration
     fn update_state(&mut self) {
         self.s[0] = 2. * self.vout[0] - self.s[0];
         self.s[1] = 2. * self.vout[1] - self.s[1];
         self.s[2] = 2. * self.vout[2] - self.s[2];
         self.s[3] = 2. * self.vout[3] - self.s[3];
     }
-    //performs a complete filter process (mystran's method)
+    // performs a complete filter process (mystran's method)
     fn tick_pivotal(&mut self, input: f32) {
-        if self.drive > 0. {
-            self.run_ladder_nonlinear(input * (self.drive + 0.7), Method::Pivotal);
+        if self.params.drive.get() > 0. {
+            self.run_ladder_nonlinear(input * (self.params.drive.get() + 0.7));
         } else {
             //
-            self.run_ladder_nonlinear(input, Method::Linear);
+            self.run_ladder_linear(input);
         }
         self.update_state();
     }
-    //instead of proper nonlinearities, this just has soft-clipping on the input
-    fn _tick_simple(&mut self, input: f32) {
-        if self.drive > 0. {
-            self.run_ladder_simple(input * (self.drive + 0.7));
-        }
-        else {
-            self.run_ladder_nonlinear(input, Method::Linear);
-        }
-        self.update_state();
-    }
-    fn run_ladder_simple(&mut self, input: f32) {
-        let x = input.tanh();
-        //denominators of solutions of individual stages. Simplifies the math a bit
-            let g0 = 1. / (1. + self.g);
-            let g1 = self.g * g0 * g0;
-            let g2 = self.g * g1 * g0;
-            let g3 = self.g * g2 * g0;
-            //outputs a 24db filter
-            self.vout[3] = (g3 * self.g * x
-                + g0 * self.s[3]
-                + g1 * self.s[2]
-                + g2 * self.s[1]
-                + g3 * self.s[0])
-                / (g3 * self.g * self.res + 1.);
-            //since we know the feedback, we can solve the remaining outputs:
-            self.vout[0] = g0 * (self.g * (x - self.res * self.vout[3]) + self.s[0]);
-            self.vout[1] = g0 * (self.g * self.vout[0] + self.s[1]);
-            self.vout[2] = g0 * (self.g * self.vout[1] + self.s[2]);
-    }
-    //nonlinear ladder filter function.  
-    fn run_ladder_nonlinear(&mut self, input: f32, method: Method) {
+    // nonlinear ladder filter function with distortion.
+    fn run_ladder_nonlinear(&mut self, input: f32) {
         let mut a = [1f32; 5];
-        //version with drive
-        if method == Method::Pivotal {
-            let base = [
-                0.,//self.res * self.s[3],
-                self.s[0],
-                self.s[1],
-                self.s[2],
-                self.s[3],
-            ];
-            //a[n] is the fixed-pivot approximation for tanh()
-            for n in 0..base.len() {
-                if base[n] != 0. {
-                    a[n] = base[n].tanh() / base[n];
-                } else {
-                    a[n] = 1.;
-                }
+        let base = [input, self.s[0], self.s[1], self.s[2], self.s[3]];
+        // a[n] is the fixed-pivot approximation for tanh()
+        for n in 0..base.len() {
+            if base[n] != 0. {
+                a[n] = base[n].tanh() / base[n];
+            } else {
+                a[n] = 1.;
             }
-            //denominators of solutions of individual stages. Simplifies the math a bit
-            let g0 = 1. / (1. + self.g * a[1]); let g1 = 1. / (1. + self.g * a[2]);
-            let g2 = 1. / (1. + self.g * a[3]); let g3 = 1. / (1. + self.g * a[4]);
-            // these are just factored out of the feedback solution. Makes the math way easier to read
-            let f3 = self.g * a[3] * g3; let f2 = self.g * a[2] * g2 * f3;
-            let f1 = self.g * a[1] * g1 * f2; let f0 = self.g * g0 * f1;
-            //outputs a 24db filter
-            self.vout[3] = (f0 * input + f1 * g0 * self.s[0]
-                + f2 * g1 * self.s[1]
-                + f3 * g2 * self.s[2]
-                + g3 * self.s[3])
-                / (f0 * self.res * a[3] + 1.);
-            //since we know the feedback, we can solve the remaining outputs:
-            self.vout[0] =
-                g0 * (self.g * a[1] * (input - self.res * a[3] * self.vout[3]) + self.s[0]);
-            self.vout[1] = g1 * (self.g * a[2] * self.vout[0] + self.s[1]);
-            self.vout[2] = g2 * (self.g * a[3] * self.vout[1] + self.s[2]);
         }
-        //linear version without drive
-        else {
-            //denominators of solutions of individual stages. Simplifies the math a bit
-            let g0 = 1. / (1. + self.g);
-            let g1 = self.g * g0 * g0;
-            let g2 = self.g * g1 * g0;
-            let g3 = self.g * g2 * g0;
-            //outputs a 24db filter
-            self.vout[3] = (g3 * self.g * input
-                + g0 * self.s[3]
-                + g1 * self.s[2]
-                + g2 * self.s[1]
-                + g3 * self.s[0])
-                / (g3 * self.g * self.res + 1.);
-            //since we know the feedback, we can solve the remaining outputs:
-            self.vout[0] = g0 * (self.g * (input - self.res * self.vout[3]) + self.s[0]);
-            self.vout[1] = g0 * (self.g * self.vout[0] + self.s[1]);
-            self.vout[2] = g0 * (self.g * self.vout[1] + self.s[2]);
-        }
+        // denominators of solutions of individual stages. Simplifies the math a bit
+        let g0 = 1. / (1. + self.params.g.get() * a[1]);
+        let g1 = 1. / (1. + self.params.g.get() * a[2]);
+        let g2 = 1. / (1. + self.params.g.get() * a[3]);
+        let g3 = 1. / (1. + self.params.g.get() * a[4]);
+        //  these are just factored out of the feedback solution. Makes the math way easier to read
+        let f3 = self.params.g.get() * a[3] * g3;
+        let f2 = self.params.g.get() * a[2] * g2 * f3;
+        let f1 = self.params.g.get() * a[1] * g1 * f2;
+        let f0 = self.params.g.get() * g0 * f1;
+        // outputs a 24db filter
+        self.vout[3] = (f0 * input * a[0]
+            + f1 * g0 * self.s[0]
+            + f2 * g1 * self.s[1]
+            + f3 * g2 * self.s[2]
+            + g3 * self.s[3])
+            / (f0 * self.params.res.get() * a[3] + 1.);
+        // since we know the feedback, we can solve the remaining outputs:
+        self.vout[0] = g0
+            * (self.params.g.get()
+                * a[1]
+                * (input * a[0] - self.params.res.get() * a[3] * self.vout[3])
+                + self.s[0]);
+        self.vout[1] = g1 * (self.params.g.get() * a[2] * self.vout[0] + self.s[1]);
+        self.vout[2] = g2 * (self.params.g.get() * a[3] * self.vout[1] + self.s[2]);
+    }
+    // linear version without distortion
+    pub fn run_ladder_linear(&mut self, input: f32) {
+        // denominators of solutions of individual stages. Simplifies the math a bit
+        let g0 = 1. / (1. + self.params.g.get());
+        let g1 = self.params.g.get() * g0 * g0;
+        let g2 = self.params.g.get() * g1 * g0;
+        let g3 = self.params.g.get() * g2 * g0;
+        // outputs a 24db filter
+        self.vout[3] = (g3 * self.params.g.get() * input
+            + g0 * self.s[3]
+            + g1 * self.s[2]
+            + g2 * self.s[1]
+            + g3 * self.s[0])
+            / (g3 * self.params.g.get() * self.params.res.get() + 1.);
+        // since we know the feedback, we can solve the remaining outputs:
+        self.vout[0] =
+            g0 * (self.params.g.get() * (input - self.params.res.get() * self.vout[3]) + self.s[0]);
+        self.vout[1] = g0 * (self.params.g.get() * self.vout[0] + self.s[1]);
+        self.vout[2] = g0 * (self.params.g.get() * self.vout[1] + self.s[2]);
     }
 }
-//default values for parameters
-impl Default for LadderFilter {
-    fn default() -> LadderFilter {
-        LadderFilter {
-            vout: [0f32; 4],
-            s: [0f32; 4],
-            sample_rate: 88200.,
-            cutoff: 1000.,
-            res: 2.0,
-            g: 0.07135868087,
-            poles: 3,
-            drive: 0.,
-        }
+impl LadderParameters {
+    pub fn set_cutoff(&self, value: f32) {
+        // cutoff formula gives us a natural feeling cutoff knob that spends more time in the low frequencies
+        self.cutoff.set(20000. * (1.8f32.powf(10. * value - 10.)));
+        // bilinear transformation for g gives us a very accurate cutoff
+        self.g
+            .set((PI * self.cutoff.get() / (self.sample_rate.get())).tan());
+    }
+    pub fn get_poles(&self) -> f32 {
+        self.poles.load(Ordering::Relaxed) as f32 + 1.
     }
 }
-
-impl Plugin for LadderFilter
-{
-    fn set_sample_rate(&mut self, rate: f32) {
-        self.sample_rate = rate;
-    }
-    fn get_info(&self) -> Info
-    {
-        Info  {
-            name: "LadderFilter".to_string(),
-            unique_id: 9263,
-            inputs: 1,
-            outputs: 1,
-            category: Category::Effect,
-            parameters: 4,
-            ..Default::default()
-        }
-    }
+impl PluginParameters for LadderParameters {
     fn get_parameter(&self, index: i32) -> f32 {
-    match index {
-        0 => self.cutoff,
-        1 => self.res,
-        2 => (self.poles) as f32 + 1.,
-        3 => self.drive,
-        _ => 0.0,
+        match index {
+            0 => self.cutoff.get(),
+            1 => self.res.get(),
+            2 => self.get_poles(),
+            3 => self.drive.get(),
+            _ => 0.0,
         }
     }
-    fn set_parameter(&mut self, index: i32, value: f32) {
+    fn set_parameter(&self, index: i32, value: f32) {
         match index {
             0 => self.set_cutoff(value),
-            1 => self.res = value * 4.,
-            2 => self.poles = ((value * 3.).round()) as usize,
-            3 => self.drive = value * 5.,
+            1 => self.res.set(value * 4.),
+            2 => self
+                .poles
+                .store(((value * 3.).round()) as usize, Ordering::Relaxed),
+            3 => self.drive.set(value * 5.),
             _ => (),
         }
     }
@@ -223,14 +186,55 @@ impl Plugin for LadderFilter
             _ => "".to_string(),
         }
     }
+    // This is what will display underneath our control.  We can
+    // format it into a string that makes the most sense.
+    fn get_parameter_text(&self, index: i32) -> String {
+        match index {
+            0 => format!("{:.0}", self.cutoff.get()),
+            1 => format!("{:.3}", self.res.get()),
+            2 => format!("{}", self.get_poles()),
+            3 => format!("{:.3}", self.drive.get()),
+            _ => format!(""),
+        }
+    }
+}
+
+// default values for parameters
+impl Default for LadderFilter {
+    fn default() -> LadderFilter {
+        LadderFilter {
+            vout: [0f32; 4],
+            s: [0f32; 4],
+            params: Arc::new(LadderParameters::default()),
+        }
+    }
+}
+impl Plugin for LadderFilter {
+    fn set_sample_rate(&mut self, rate: f32) {
+        self.params.sample_rate.set(rate);
+    }
+    fn get_info(&self) -> Info {
+        Info {
+            name: "LadderFilter".to_string(),
+            unique_id: 9263,
+            inputs: 1,
+            outputs: 1,
+            category: Category::Effect,
+            parameters: 4,
+            ..Default::default()
+        }
+    }
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
         for (input_buffer, output_buffer) in buffer.zip() {
             for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
                 self.tick_pivotal(*input_sample);
-                //the poles parameter chooses which filter stage we take our output from.
-                *output_sample = self.vout[self.poles];
+                // the poles parameter chooses which filter stage we take our output from.
+                *output_sample = self.vout[self.params.poles.load(Ordering::Relaxed)];
             }
         }
+    }
+    fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
+        Arc::clone(&self.params) as Arc<dyn PluginParameters>
     }
 }
 plugin_main!(LadderFilter);
