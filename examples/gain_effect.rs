bb73b94b638011f5d6fdf79942b543da4e36cae7
Charles Saracco <crsaracco@gmail.com>
2019-11-04 22:24:46 +0100

    Make CI fail if Clippy finds any warnings (#104)

--- examples/gain_effect.rs
+++ examples/gain_effect.rs
@@ -61,7 +61,7 @@ impl Plugin for GainEffect {
             name: "Gain Effect in Rust".to_string(),
             vendor: "Rust DSP".to_string(),
             unique_id: 243723072,
-            version: 0001,
+            version: 1,
             inputs: 2,
             outputs: 2,
             // This `parameters` bit is important; without it, none of our
@@ -106,6 +106,7 @@ impl PluginParameters for GainEffectParameters {
 
     // the `set_parameter` function sets the value of a parameter.
     fn set_parameter(&self, index: i32, val: f32) {
+        #[allow(clippy::single_match)]
         match index {
             0 => self.amplitude.set(val),
             _ => (),
