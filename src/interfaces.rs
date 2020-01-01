301d18d29dfe269f5e21c6d2fb21cad26b1bc245
Ivan Nikulin <ifaaan@gmail.com>
2020-01-01 21:53:10 +0000

    Fix `EditorGetRect` opcode
    Plugin should return `1` if it has editor on `EditorGetRect` opcode, otherwise provided dimensions won't be applied by host.

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -135,6 +135,8 @@ pub fn dispatch(effect: *mut AEffect, opcode: i32, index: i32, value: isize, ptr
                         bottom: (pos.1 + size.1) as i16, // y coord of pos + y coord of size
                     })) as *mut _; // TODO: free memory
                 }
+                
+                return 1;
             }
         }
         OpCode::EditorOpen => {
