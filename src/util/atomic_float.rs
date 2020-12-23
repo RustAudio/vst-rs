16ecf40f67cbd84d5376c43c2658c96f807abb0e
Aaron Kofsky <aaronko@umich.edu>
2020-12-23 00:27:54 -0500

    Implement Debug and From<f32> for AtomicFloat
    Fixes #149

--- src/util/atomic_float.rs
+++ src/util/atomic_float.rs
@@ -33,3 +33,15 @@ impl Default for AtomicFloat {
         AtomicFloat::new(0.0)
     }
 }
+
+impl std::fmt::Debug for AtomicFloat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt(&self.get(), f)
+    }
+}
+
+impl From<f32> for AtomicFloat {
+    fn from(value: f32) -> Self {
+        AtomicFloat::new(value)
+    }
+}
