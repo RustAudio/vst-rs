dc7e29f08a66bd798fea8dbbf6ebaa4cfbc17165
Boscop <Boscop@users.noreply.github.com>
2020-12-23 22:46:04 +0000

    deploy: 02847c733c7541996054df62d86bf29af1cebdfd

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["vst"] = [{"text":"impl Display for PluginLoadError","synthetic":false,"types":[]}];
+implementors["vst"] = [{"text":"impl Display for PluginLoadError","synthetic":false,"types":[]},{"text":"impl Display for AtomicFloat","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
