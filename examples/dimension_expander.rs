fc54a30a02afe414203d67cad73ae40c4dbcb193
tpltnt <1172976+tpltnt@users.noreply.github.com>
2021-02-27 22:18:51 +0100

    updated time to 0.2

--- examples/dimension_expander.rs
+++ examples/dimension_expander.rs
@@ -11,6 +11,7 @@ use vst::util::AtomicFloat;
 use std::collections::VecDeque;
 use std::f64::consts::PI;
 use std::sync::Arc;
+use time::OffsetDateTime;
 
 /// Calculate the length in samples for a delay. Size ranges from 0.0 to 1.0.
 fn delay(index: usize, mut size: f32) -> isize {
@@ -155,7 +156,7 @@ impl Plugin for DimensionExpander {
             let mut right_processed = 0.0;
 
             // Recalculate time per sample
-            let time_s = time::precise_time_ns() as f64 / 1_000_000_000.0;
+            let time_s = (OffsetDateTime::now_utc() - OffsetDateTime::unix_epoch()).as_seconds_f64();
 
             // Use buffer index to offset volume LFO
             for (n, buffer) in self.buffers.iter_mut().enumerate() {
