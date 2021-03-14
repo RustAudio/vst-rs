ffc4c43db7962ef57b8ecc0a90f65bcabb5937d0
Nico Chatzi <nico.chatzigianis@focusrite.com>
2021-03-14 12:03:12 +0000

    Remove crate:: in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,12 +1,12 @@
-//! A collection of commonly used items
+//! A collection of commonly used items for implement a Plugin
 
 #[doc(no_inline)]
-pub use crate::api::{Events, Supported};
+pub use api::{Events, Supported};
 #[doc(no_inline)]
-pub use crate::buffer::{AudioBuffer, SendEventBuffer};
+pub use buffer::{AudioBuffer, SendEventBuffer};
 #[doc(no_inline)]
-pub use crate::event::{Event, MidiEvent};
+pub use event::{Event, MidiEvent};
 #[doc(no_inline)]
-pub use crate::plugin::{CanDo, Category, HostCallback, Info, Plugin, PluginParameters};
+pub use plugin::{CanDo, Category, HostCallback, Info, Plugin, PluginParameters};
 #[doc(no_inline)]
-pub use crate::util::{AtomicFloat, ParameterTransfer};
+pub use util::{AtomicFloat, ParameterTransfer};
