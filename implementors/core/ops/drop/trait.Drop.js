982bd79028406d9d7dc131ef8d311421885bbf97
glowcoil <glowcoil@users.noreply.github.com>
2021-07-30 18:17:21 +0000

    deploy: 822e1d3b53666226b09e2c532e16a3dbe87a8fb5

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["vst"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"vst/host/struct.PluginInstance.html\" title=\"struct vst::host::PluginInstance\">PluginInstance</a>","synthetic":false,"types":["vst::host::PluginInstance"]}];
+implementors["vst"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"vst/host/struct.PluginInstance.html\" title=\"struct vst::host::PluginInstance\">PluginInstance</a>","synthetic":false,"types":["vst::host::PluginInstance"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
