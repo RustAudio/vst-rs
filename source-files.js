6c94152faa5330fa960c7d79a64b2d58681cec4e
piedoom <piedoom@users.noreply.github.com>
2022-02-26 16:32:22 +0000

    deploy: c3acb39d84c3c8fe6153e32b5cb2f645095921dc

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["vst"] = {"name":"","dirs":[{"name":"util","files":["atomic_float.rs","mod.rs","parameter_transfer.rs"]}],"files":["api.rs","buffer.rs","cache.rs","channels.rs","editor.rs","event.rs","host.rs","interfaces.rs","lib.rs","plugin.rs"]};
+sourcesIndex["vst"] = {"name":"","dirs":[{"name":"util","files":["atomic_float.rs","mod.rs","parameter_transfer.rs"]}],"files":["api.rs","buffer.rs","cache.rs","channels.rs","editor.rs","event.rs","host.rs","interfaces.rs","lib.rs","plugin.rs","prelude.rs"]};
 createSourceSidebar();
